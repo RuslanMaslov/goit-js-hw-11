4cc91b8a74589c23e70359a80adc8496ffe83d3e
Ruslan Maslov <oblivion475@gmail.com>
2024-07-30 23:17:17 +0300

    key

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,5 +1,6 @@
 const params = {
-    key: "44868701-a6c2bf036a020cab1604664f3",
+    key: "45099843-4e55e9d150b17e0e0ee4548df",
+    // 44868701-a6c2bf036a020cab1604664f3
     q: "",
     imageType: "photo",
     orientation: "horizontal",
