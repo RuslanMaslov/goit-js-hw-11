83539558b83a2570a6a6497344b52bffd3f81f5f
Ruslan Maslov <oblivion475@gmail.com>
2024-07-30 22:30:58 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,9 +1,15 @@
-const apiKey = '44813412-f6fc02e89419494116d973502';
+export function searchImages(query) {
+const BASE_URL = 'https://pixabay.com/api/';   
+const api_Key = '45099843-4e55e9d150b17e0e0ee4548df';
+const imgType = 'photo';  
+const orient = 'horizontal';
+const safesearch = 'tru'; 
 
-export async function fetchImages(query) {
-  const response = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`);
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-  return await response.json();
+// export async function fetchImages(query) {
+//   const response = await fetch(`https://pixabay.com/api/?key=${apiKey}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true`);
+//   if (!response.ok) {
+//     throw new Error('Network response was not ok');
+//   }
+//   return await response.json();
+return   
 }
